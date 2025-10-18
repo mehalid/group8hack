9ccf5549e4f99b92dd53ba52d89a2d30c91f488d
Andrew_Lin <al885@cornell.edu>
2025-10-18 13:27:22 -0400

    Document generate_solar_report function
    Added documentation and export tag for generate_solar_report function.

--- R/generate_report.R
+++ R/generate_report.R
@@ -1,3 +1,12 @@
+#' @name generate_solar_report
+#' @title generate_solar_report
+#' @description Function to generate a website with our graphs and data analysis
+#' @author group8
+#' @params stats, bar_plot, var_plot, output_file
+#' @note Adding `@export` below means this function will become accessible by package users, rather than being an internal-only function.
+#' @export
+
+
 generate_solar_report <- function(stats, bar_plot, var_plot, 
                                   output_file = "Solar_Report.html") {
   rmd_path <- normalizePath("R/report.Rmd", mustWork = TRUE)
