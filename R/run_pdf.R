1c93d49d47cfb5358b6e857ad1c5a042c757261b
JennieRedrovan44 <Jennie.redrovan18@gmail.com>
2025-10-18 01:48:39 -0400

    Replace write_panel_report with write_single_page_report

---
+++ R/run_pdf.R
@@ -0,0 +1,9 @@
+# Assuming you already have:
+#   stats  <- panel_stats("R/panel_life_NY001.csv", ...)
+#   uout   <- make_failure_uchart(panel_life_NY001, ...)
+#   u_plot <- uout$plot
+
+# you already have: stats <- panel_stats(...), and u_plot from your u-chart function
+write_single_page_report(stats, g1,
+                         out_pdf = "SolarPanel_ReliabilityReport.pdf",
+                         warranty_years = 25)
